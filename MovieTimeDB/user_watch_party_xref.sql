7873a5071ac8d8498ed629c11ff4e9ec8852ecb5
ktagseth88 <41531405+ktagseth88@users.noreply.github.com>
2019-04-27 19:21:38 -0400

    DB schema changes for watch_party functionality

---
+++ MovieTimeDB/user_watch_party_xref.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[user_watch_party_xref]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[user_id] INT NOT NULL,
+	[watch_party_id] INT NOT NULL
+)
+GO
+
+
+ALTER TABLE dbo.user_watch_party_xref
+ADD CONSTRAINT FK_user_user_watch_party_xref FOREIGN KEY ([user_id]) REFERENCES [dbo].[user]([user_id])
+GO
+
+ALTER TABLE dbo.user_watch_party_xref
+ADD CONSTRAINT FK_watch_party_user_watch_party_xref FOREIGN KEY ([watch_party_id]) REFERENCES [dbo].[watch_party]([watch_party_id])
+GO
---
+++ MovieTimeDB/watch_party.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[watch_party]
+(
+	[watch_party_id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[name] VARCHAR(255) NULL
+)
